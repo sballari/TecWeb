0de72764f344a05ff35434e2cd80bb5291a75b91
sballari <ballarinsimone@gmail.com>
2018-01-04 22:35:35 +0100

    inserito scheletro manipulator

---
+++ Src/php/class/Manipulator.php
@@ -0,0 +1,24 @@
+<?php
+class Manipulator {
+  private $dbM;
+  function insertUser($User){
+    return false;
+  }
+  function insertProduct($product){
+    return false;
+  }
+  function removeUser($user){
+    return false;
+  }
+  function removeProduct($product){
+    return false;
+  }
+  function insertRequest($Request){
+
+  }
+  function removeRequest($Request){
+    return false;
+  }
+
+}
+?>
