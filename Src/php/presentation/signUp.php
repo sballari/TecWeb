a3f46dbc48133919b1b4d7279d0169b69e45e50a
GertaLleshi <34062758+GertaLleshi@users.noreply.github.com>
2018-06-23 14:20:14 +0200

    Messo apposto il signup

--- Src/php/presentation/signUp.php
+++ Src/php/presentation/signUp.php
@@ -2,126 +2,122 @@
 <html lang ="it">
   <?php
       require_once("CommonHtmlElement.php");
-      require_once("../services/Factory.php");
-      require_once("../models/User.php");
-      require_once("../services/Manipulator.php");
       require_once("../services/DBmanager.php");
+      require_once("../models/User.php");
+      require_once("../services/Factory.php");
       $h = new CommonHtmlElement();
+      $h->printHead("SignUp", "area personale", "login, signup");
+  ?>
+  <body onload='creaStatistiche()'>
+    <?php
+	    session_start();
       $d = new DBmanager("localhost", "root", "", "i_tesori_di_squitty_mod");
       $d->connect();
       $f = new Factory($d);
-      $h->printHead("singUp", "area personale", "login, signup");
- ?>
- <body onload='creaStatistiche()'>
-   <?php
-	   session_start();
-     if(isset($_SESSION['Email'])){
-       $u = $f->getUser($_SESSION['Email']);
-       $t = $u->getUserType();
-       if($t == "Impiegato"){
-         header("Location: areaPersonaleImpiegato.php");
-       }
-       else{
-         header("Location: areaPersonale.php");
-       }
-     }
-	   $h->createheader("signUp");
-     $h->printInternalMenu("signUp");
-   ?>
-
-   <div id="content">
-     <div id="info" class="contentElement">
-    		<h3>INFO</h3>
-			  <p>
-        Se disponi gi&agrave di un account prego procedere all'accesso dal seguente bottone.
+      if(isset($_SESSION['Email'])){
+        $u = $f->getUser($_SESSION['Email']);
+        $t = $u->getUserType();
+        if($t == "Impiegato"){
+          header("Location: areaPersonaleImpiegato.php");
+        }
+        else{
+          header("Location: areaPersonale.php");
+        }
+      }
+	    $h->createheader("SignUp");
+      $h->printInternalMenu("SignUp");
+      $d->disconnect();
+    ?>
+    <div id="content">
+      <div id="info" class="contentElement">
+      	<h3>INFO</h3>
+        <p>Se disponi gi&agrave di un account proccedere al seguente bottone.
         </br>
-			  <a href="logIn.php">Vai alla pagina di <span lang="en">Log in</span></a>.
+          <a href="logIn.php">Vai alla pagina di  <span lang="en">Log in</span></a>.
         </p>
+      </div>
+      <div id="form" class="contentElement">
+      	<form action="../operationManagers/signUpOperationManager.php" method="POST">
+      	<fieldset>
+        	<legend>Creazione account:</legend>
+
+        	<p><span class="err"><?php
+          if(isset($_SESSION["ErrSignup"])){
+          echo $_SESSION["ErrSignup"]."</br>";
+          unset($_SESSION["ErrSignup"]);
+          } ?></span></p>
+
+          <div id="nome">
+          <label for="Nome">Nome: </label>
+        	<input type="text" id="Nome" name="nome" placeholder="insert your name" value="<?php $datiIn=array();
+          if(isset($_SESSION["datiIn"])){
+          $datiIn=unserialize($_SESSION["datiIn"]);
+          echo $datiIn[0]."";
+          }?>" required><span class="err">
+          <?php if(isset($_SESSION["ErrNome"])){
+          echo $_SESSION["ErrNome"];
+          unset($_SESSION["ErrNome"]);
+          } ?></span>
+          </div>
+
+          <div id="cognome">
+          <label for="Cognome">Cognome: </label>
+        	<input type="text" id="Cognome" name="cognome" placeholder="insert your surname" value="<?php if(isset($_SESSION["datiIn"])){
+          echo $datiIn[1]."";
+          }?>" required ><span class="err">
+          <?php if(isset($_SESSION["ErrCognome"])){
+          echo $_SESSION["ErrCognome"];
+          unset($_SESSION["ErrCognome"]);
+          } ?></span>
+          </div>
+          </br>
+
+          <div id="tipo">
+            <label for="tipoUtente">Tipo utente</label>
+            <select name="tipoUtente" required>
+              <option value="">--</option>
+              <option value="Al minuto">Al minuto</option>
+              <option value="All_ingrosso">All'ingrosso</option>
+              <option value="Servizio">Servizio</option>
+              <option value="Impiegato">Impiegato</option>
+            </select>
+          </div>
+
+          <div id="email">
+          <label for="Email">Email: </label>
+        	<input type="email" id="Email" name="emailSignup" placeholder="mickey.mouse@gmail.com" value="<?php
+          if(isset($_SESSION["datiIn"])){
+          echo $datiIn[3]."";
+          }?>" required><span class="err">
+          <?php if(isset($_SESSION["ErrEmail"])){
+          echo $_SESSION["ErrEmail"];
+          unset($_SESSION["ErrEmail"]);
+          } ?></span>
+          </div>
+
+          <div id="password">
+          <label for="Password">Password: </label>
+        	<input type="password" id="Password" name="passwordSignup" placeholder="insert your password" value="<?php if(isset($_SESSION["datiIn"])){
+          echo $datiIn[4]."";
+          unset($_SESSION["datiIn"]);
+          }?>" required ><span class="err">
+          <?php if(isset($_SESSION["ErrPassword"])){
+          echo $_SESSION["ErrPassword"];
+          unset($_SESSION["ErrPassword"]);
+          } ?></span>
+          </div>
+          </br>
+        	<button type="submit" name = "createAccount" >Create account</button>
+      	</fieldset>
+      	</form>
+    	</div>
     </div>
-	  <div class="contentElement">
-				<form id="form" action="../operationManagers/signUpOperationManager.php"  method="POST">
-					<fieldset>
-            <legend>Creazione account:</legend>
-            <p><span class="err"><?php
-            if(isset($_SESSION["ErrSignup"])){
-            echo $_SESSION["ErrSignup"]."</br>";
-            unset($_SESSION["ErrSignup"]);
-            } ?></span></p>
 
-            <div id="nome">
-  						<label for="Nome">Nome: </label>
-  						<input type="text" name="nome" id="Nome" placeholder="insert your name"  value="<?php $datiInseriti=array();
-              if(isset($_SESSION["datiIn"])){
-              $datiInseriti=unserialize($_SESSION["datiIn"]);
-              echo $datiInseriti[0]."";
-              }?>" required><span class="err">
-              <?php
-              if(isset($_SESSION["ErrNome"])){
-              echo $_SESSION["ErrNome"]."</br>";
-              unset($_SESSION["ErrNome"]);
-              } ?></span>
-              </div>
-
-              <div id="cognome">
-  						<label for="Cognome">Cognome: </label>
-  						<input type="text" id="Cognome" name="cognome"  placeholder="insert your surname"  value="<?php if(isset($_SESSION["datiIn"])){
-              echo $datiInseriti[1]."";
-              }?>" required><span class="err">
-              <?php
-              if(isset($_SESSION["ErrCognome"])){
-              echo $_SESSION["ErrCognome"]."</br>";
-              unset($_SESSION["ErrCognome"]);
-              } ?><span>
-              </div>
-
-              <div id="tipo">
-  						<label for="tipoUtente">Tipo utente: </label>
-  						<select name="tipoUtente" required>
-  							<option value="">--</option>
-  							<option value="Al minuto">Al minuto</option>
-  							<option value="All ingrosso">All'ingrosso</option>
-  							<option value="Servizio">Servizio</option>
-  							<option value="Impiegato">Impiegato</option>
-  							</select>
-              </div>
-
-              <div id="email">
-  						<label for="Email">Email: </label>
-  						<input type="email" id="Email" name="emailSignup" placeholder="mickey.mouse@gmail.com"  value="<?php if(isset($_SESSION["datiIn"])){
-              echo $datiInseriti[3]."";
-              }?>" required><span class="err">
-              <?php
-              if(isset($_SESSION["ErrEmail"])){
-              echo $_SESSION["ErrEmail"]."</br>";
-              unset($_SESSION["ErrEmail"]);
-              } ?></span>
-              </div>
-
-              <div id="password">
-  						<label for="Password">Password: </label>
-  						<input type="password" id="Password" name="passwordSignup" placeholder="insert your password"  value="<?php if(isset($_SESSION["datiIn"])){
-              echo $datiInseriti[4]."";
-              unset($_SESSION["datiIn"]);
-              }?>" required><span class="err">
-              <?php
-              if(isset($_SESSION["ErrPassword"])){
-              echo $_SESSION["ErrPassword"]."</br>";
-              unset($_SESSION["ErrPassword"]);
-              } ?></span>
-              </div>
-
-						<button type="submit" name="createAccount">Create account</button>
-					</fieldset>
-				</form>
-			</div>
-    </div>
-
-    <?php
-	    $h->createStatisticDiv();
+		<?php
+		  $h->createStatisticDiv();
       $h->printContatti();
       $h->printFooter();
-      $h->printMobileMenu("singUp");
-      $d->disconnect();
+      $h->printMobileMenu("signUp");
     ?>
 
   </body>
